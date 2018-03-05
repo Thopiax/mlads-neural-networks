adc2c73291e8e60a7a078efc4134fa9d9402578f
Rafael Ballestiero <rafa.ballestiero@gmail.com>
2018-03-05 14:31:57 +0000

    Added comments to test hidden layers and removed data loading logic

--- main.py
+++ main.py
@@ -1,78 +1,58 @@
 from model import Model
-from scipy.io import loadmat
+from data import load_data
+
 from random import randint
 import csv
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import SGD
 from keras.layers import LeakyReLU
 from keras.utils import plot_model
 
-class InputData:
-    def __init__(self, data, targets):
-        assert len(data) == len(targets)
-
-        self.data = data
-        self.targets = targets
-
-    def __len__(self):
-        return len(self.data)
-
-
-def load_data():
-    data = loadmat("data4students.mat")
-    inputs = data["datasetInputs"][0]
-    targets = data["datasetTargets"][0]
-
-    training_data = InputData(inputs[0], targets[0])
-    test_data = InputData(inputs[1], targets[1])
-    validation_data = InputData(inputs[2], targets[2])
-
-    assert len(training_data) == 25120
-    assert len(test_data) == 3589
-    assert len(validation_data) == 3589
-
-    return training_data, test_data, validation_data
-
-
 def main():
-    training_data, test_data, validation_data = load_data()
-
-    # def test_param_values(name, low, high, step=1, ratio=1):
-    #     for i in range(low, high, step):
-    #         print("TESTING PARAM VALUES FOR {}={}".format(name, i/ratio))
-    #         model = Model(training_data, test_data, {name: i/ratio})
-    #         model.build()
-    #         model.train(epochs=500, batch_size=128)
-    #         model.evaluate()
+    training_data, test_data, validation_data = load_data("data4students.mat")
 
     with open("nn-history.csv", "a") as csvfile:
         writer = csv.writer(csvfile)
 
-        for i in range(100):
-            model = Model(training_data, test_data)
-            model.model = Sequential()
+        print("Creating model...")
+        model = Model(training_data, test_data)
+        print("Building model...")
+        model.build()
+        print("Training model...")
+        model.train()
+        print("Evaluating model...")
+        model.evaluate()
 
-            hidden_layers = randint(3, 8)
-            layer_neurons = [randint(0, 2000) for x in range(hidden_layers + 1)]
-
-            print("Number of hidden layers: {}".format(hidden_layers))
-            print("Neuron layout: {}".format(layer_neurons))
-
-            model.model.add(Dense(layer_neurons[0], input_dim=900))
-            model.model.add(LeakyReLU(alpha=0.5))
-            for i in range(0, hidden_layers):
-                model.model.add(Dense(layer_neurons[i + 1]))
-                model.model.add(LeakyReLU(alpha=0.5))
-
-            model.model.add(Dense(7, activation='softmax'))
-
-            plot_model(model.model, show_shapes=True, to_file='models/model-{}.png'.format(i))
-
-            model.train(epochs=1000, batch_size=128)
-            loss, accuracy = model.evaluate()
-
-            writer.writerow([i, hidden_layers, layer_neurons, accuracy, loss])
+        # for i in range(100):
+        #     print("Creating model...")
+        #     model = Model(training_data, test_data)
+        #     print("Building model...")
+        #     model.model = Sequential()
+        #
+        #     hidden_layers = randint(3, 8)
+        #     layer_neurons = [randint(0, 2000) for x in range(hidden_layers + 1)]
+        #
+        #     print("Number of hidden layers: {}".format(hidden_layers))
+        #     print("Neuron layout: {}".format(layer_neurons))
+        #
+        #     model.model.add(Dense(900, input_dim=900, activation='linear'))
+        #
+        #     for i in range(0, hidden_layers):
+        #         model.model.add(Dense(layer_neurons[i + 1]))
+        #         model.model.add(LeakyReLU(alpha=0.5))
+        #
+        #     model.model.add(Dense(7, activation='softmax'))
+        #
+        #     plot_model(model.model, show_shapes=True, to_file='models/model-{}.png'.format(i))
+        #
+        #     print("Training model...")
+        #     model.train(epochs=100, batch_size=128)
+        #     print("Evaluating model...")
+        #     loss, accuracy = model.evaluate()
+        #
+        #     writer.writerow([i, hidden_layers, layer_neurons, accuracy, loss])
 
 
 if __name__ == "__main__":
