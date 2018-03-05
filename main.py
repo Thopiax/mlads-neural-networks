3ccd6803c17e5703a403bad9cb127cca60d45020
Norbert Podsadowski <norbo11@googlemail.com>
2018-03-05 17:35:09 +0000

    Remove .csv stuff

--- main.py
+++ main.py
@@ -22,46 +22,35 @@ def main():
             model.evaluate()
             print()
 
-    with open("nn-history.csv", "a") as csvfile:
-        writer = csv.writer(csvfile)
-
+    for i in range(100):
         print("Creating model...")
         model = Model(training_data, test_data)
         print("Building model...")
-        model.build()
-        print("Training model...")
-        model.train()
-        print("Evaluating model...")
-        model.evaluate()
+        model.model = Sequential()
 
-        # for i in range(100):
-        #     print("Creating model...")
-        #     model = Model(training_data, test_data)
-        #     print("Building model...")
-        #     model.model = Sequential()
-        #
-        #     hidden_layers = randint(3, 8)
-        #     layer_neurons = [randint(0, 2000) for x in range(hidden_layers + 1)]
-        #
-        #     print("Number of hidden layers: {}".format(hidden_layers))
-        #     print("Neuron layout: {}".format(layer_neurons))
-        #
-        #     model.model.add(Dense(900, input_dim=900, activation='linear'))
-        #
-        #     for i in range(0, hidden_layers):
-        #         model.model.add(Dense(layer_neurons[i + 1]))
-        #         model.model.add(LeakyReLU(alpha=0.5))
-        #
-        #     model.model.add(Dense(7, activation='softmax'))
-        #
-        #     plot_model(model.model, show_shapes=True, to_file='models/model-{}.png'.format(i))
-        #
-        #     print("Training model...")
-        #     model.train(epochs=100, batch_size=128)
-        #     print("Evaluating model...")
-        #     loss, accuracy = model.evaluate()
-        #
-        #     writer.writerow([i, hidden_layers, layer_neurons, accuracy, loss])
+        hidden_layers = randint(3, 8)
+        layer_neurons = [randint(0, 2000) for x in range(hidden_layers + 1)]
+
+        print("Number of hidden layers: {}".format(hidden_layers))
+        print("Neuron layout: {}".format(layer_neurons))
+
+        model.model.add(Dense(900, input_dim=900, activation='linear'))
+
+        for i in range(0, hidden_layers):
+            model.model.add(Dense(layer_neurons[i + 1]))
+            model.model.add(LeakyReLU(alpha=0.5))
+
+        model.model.add(Dense(7, activation='softmax'))
+
+        plot_model(model.model, show_shapes=True,
+                   to_file='models/model-{}.png'.format(i))
+
+        print("Training model...")
+        model.train(epochs=100, batch_size=128)
+        print("Evaluating model...")
+        loss, accuracy = model.evaluate()
+
+        writer.writerow([i, hidden_layers, layer_neurons, accuracy, loss])
 
 
 if __name__ == "__main__":
