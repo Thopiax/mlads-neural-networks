b21d5fc53634d8f624a3b02708faa687edb6b2fd
Norbert Podsadowski <norbo11@googlemail.com>
2018-03-08 14:31:19 +0000

    Normalize data properly

--- data.py
+++ data.py
@@ -2,6 +2,7 @@ from scipy.io import loadmat
 import numpy as np
 import pickle
 import os
+from keras.utils import normalize as norm
 
 PIXELS_IN_IMAGE = 900
 TRAINING_COUNT = 25120
@@ -38,37 +39,20 @@ def calculate_mean_image(data):
 
     return mean_image
 
+
 def load_data(matlab_file):
-    if os.path.isfile("{}.pkl".format(matlab_file)):
-        print("uSing cached file")
-        with open("{}.pkl".format(matlab_file), "rb") as pkl_file:
-            return pickle.load(pkl_file)
-    else:
-        data = loadmat(matlab_file)
-        inputs = data["datasetInputs"][0]
-        targets = data["datasetTargets"][0]
+    data = loadmat(matlab_file)
+    inputs = data["datasetInputs"][0]
+    targets = data["datasetTargets"][0]
 
-        # Assign data
-        training_data   = InputData(inputs[0], targets[0])
-        testing_data    = InputData(inputs[1], targets[1])
-        validation_data = InputData(inputs[2], targets[2])
+    # Assign data
+    training_data = InputData(norm(inputs[0]), targets[0])
+    testing_data = InputData(norm(inputs[1]), targets[1])
+    validation_data = InputData(norm(inputs[2]), targets[2])
 
-        # Check data size
-        assert len(training_data) == TRAINING_COUNT
-        assert len(testing_data) == TESTING_COUNT
-        assert len(validation_data) == VALIDATION_COUNT
+    # Check data size
+    assert len(training_data) == TRAINING_COUNT
+    assert len(testing_data) == TESTING_COUNT
+    assert len(validation_data) == VALIDATION_COUNT
 
-        # Get mean image to normalize data
-        mean_image = calculate_mean_image(training_data.data)
-
-        # Normalize data
-        training_data.normalize(mean_image)
-        testing_data.normalize(mean_image)
-        validation_data.normalize(mean_image)
-
-        # Saved normalized data
-        with open("{}.pkl".format(matlab_file), "wb") as pkl_file:
-            pickle.dump((training_data, testing_data, validation_data), pkl_file)
-
-
-        return training_data, testing_data, validation_data
+    return training_data, testing_data, validation_data
